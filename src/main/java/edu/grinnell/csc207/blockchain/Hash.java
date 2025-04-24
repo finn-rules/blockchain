eaaeabd57f05eebad7c09b0db7bcb9e75f3b43d4
finn-rules <finn.rowles@gmail.com>
2025-04-24 12:10:39 -0500

    Update Hash.java - from netbeans

--- src/main/java/edu/grinnell/csc207/blockchain/Hash.java
+++ src/main/java/edu/grinnell/csc207/blockchain/Hash.java
@@ -1,28 +1,40 @@
 package edu.grinnell.csc207.blockchain;
-import java.lang.Byte;
+
 import java.util.*;
 
 /**
  * A wrapper class over a hash value (a byte array).
  */
 public class Hash {
+
     private byte[] hashData;
 
+    /**
+     * Hash : constructor for a hash
+     *
+     * @param data an array of bytes.
+     */
     public Hash(byte[] data) {
         this.hashData = data;
     }
 
+    /**
+     * getData : returns the array of bytes.
+     * @return the hash's byte array
+     */
     public byte[] getData() {
         return this.hashData;
     }
+
     /**
-     * isValid: checks whether the given hash meets the criteria for validity. 
-     * @return boolean: whether the has is valid or not
+     * isValid: checks whether the given hash meets the criteria for validity.
+     *
+     * @return (t/f): whether the hash is valid or not
      */
     public boolean isValid() {
-        return hashData[0] == 0 && 
-               hashData[1] == 0 &&
-               hashData[2] == 0;
+        return Byte.toUnsignedInt(hashData[0]) == 0
+                && Byte.toUnsignedInt(hashData[1]) == 0
+                && Byte.toUnsignedInt(hashData[2]) == 0;
         /*
         if (this.hashData == null) {
             return false;
@@ -36,12 +48,14 @@ public class Hash {
             return true;
         }
         return false;
-        */
+         */
     }
-    
+
     /**
      * toString: returns a string from the hash array using format library utils
      * String: returns a string of form hexadecimal digits, 2 digits per byte
+     *
+     * @return the string format of the hash.
      */
     @Override
     public String toString() {
@@ -49,9 +63,16 @@ public class Hash {
         for (int i = 0; i < this.hashData.length; i++) {
             s.append(String.format("%02x", hashData[i]));
         }
-        return s.toString(); 
+        return s.toString();
     }
 
+    /**
+     * equals: check to see if a hash is equal to another object.
+     *
+     * @param other : an object to compare
+     * @return (t/f) whether the object and hash are identical
+     */
+    @Override
     public boolean equals(Object other) {
         if (other instanceof Hash) {
             Hash o = (Hash) other;
@@ -59,6 +80,5 @@ public class Hash {
         }
         return false;
     }
+}
 
-
-    }
